b839dbdb701cbd9f98c0b688578d812361541b14
kat ballo <katballo@icloud.com>
2017-11-02 15:02:52 -0400

    updated

--- mergesort.js
+++ mergesort.js
@@ -1,7 +1,31 @@
-function mergeSort(){
-	split();
+function mergeSort(arr){
+
+	if(arr.length === 1){
+		return arr
+	}
+
+	else {
+		var first = split(arr)[0]
+		var second = split(arr)[1]
+
+		if (first < second) {
+			mergeSort(split(arr));
+		}
+		else{
+			mergeSort(split.([second, first])
+		}
+
+	}
+
+
 }
 
-function split(){
 
+function split(wholeArray) {
+	var firstHalf = wholeArray.slice(0, (wholeArray.length/2))
+	var secondHalf = wholeArray.slice((wholeArray.length/2))
+  	return [firstHalf, secondHalf];
 }
+
+
+console.log(split([15, 25, 5, 17]))
